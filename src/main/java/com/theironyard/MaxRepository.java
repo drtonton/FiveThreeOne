f1577a12894ee77de04ea50f6530408df38f7095
drtonton <tkfoster26@gmail.com>
2016-03-10 14:46:26 -0500

    added max/person repositories

---
+++ src/main/java/com/theironyard/MaxRepository.java
@@ -0,0 +1,9 @@
+package com.theironyard;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by noellemachin on 3/10/16.
+ */
+public interface MaxRepository extends CrudRepository<Max, Integer>{
+}
