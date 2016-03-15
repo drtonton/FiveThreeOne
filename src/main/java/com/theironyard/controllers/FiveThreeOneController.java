2a0299069f1e4eaa2a47ec095fb637d800e0f723
drtonton <tkfoster26@gmail.com>
2016-03-14 23:34:02 -0400

    notes display now.

--- src/main/java/com/theironyard/controllers/FiveThreeOneController.java
+++ src/main/java/com/theironyard/controllers/FiveThreeOneController.java
@@ -1,9 +1,11 @@
 package com.theironyard.controllers;
 import com.theironyard.entities.Calculation;
 import com.theironyard.entities.Max;
+import com.theironyard.entities.Note;
 import com.theironyard.entities.Person;
 import com.theironyard.services.CalculationRepository;
 import com.theironyard.services.MaxRepository;
+import com.theironyard.services.NoteRepository;
 import com.theironyard.services.PersonRepository;
 import com.theironyard.utils.PasswordStorage;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,132 +30,132 @@ public class FiveThreeOneController {
     @Autowired
     PersonRepository personRepository;
 
-//    @Autowired
-//    NoteRepository noteRepository;
     @Autowired
-    CalculationRepository calculationRepository;
+    NoteRepository noteRepository;
 
     @RequestMapping(path ="/", method = RequestMethod.GET)
     public String home(HttpSession session, Model model) {
         String userName = (String) session.getAttribute("userName");
         Person person = personRepository.findFirstByUserName(userName);
-//        HashMap calculations = new HashMap();
-        ArrayList<Integer> calculations = new ArrayList<>();
+        HashMap calculations = new HashMap();
         Max max = maxRepository.findFirstByPerson(person);
         if (person != null && max != null) {
-//            calculations.put("benchA1", weekOne(max.getBench()).get(0));
-//            calculations.put("benchA2", weekOne(max.getBench()).get(1));
-//            calculations.put("benchA3", weekOne(max.getBench()).get(2));
-            for (int i : weekOne(max.getBench())) {
-                Calculation calc = new Calculation(i, person);
-                calculationRepository.save(calc);
-            }
-//            calculations.put("pressA1", weekOne(max.getShoulderPress()).get(0));
-//            calculations.put("pressA2", weekOne(max.getShoulderPress()).get(1));
-//            calculations.put("pressA3", weekOne(max.getShoulderPress()).get(2));
-            for (int i : weekOne(max.getShoulderPress())) {
-                Calculation calc = new Calculation(i, person);
-                calculationRepository.save(calc);
-            }
-//            calculations.put("squatA1", weekOne(max.getSquat()).get(0));
-//            calculations.put("squatA2", weekOne(max.getSquat()).get(1));
-//            calculations.put("squatA3", weekOne(max.getSquat()).get(2));
-            for (int i : weekOne(max.getSquat())) {
-                Calculation calc = new Calculation(i, person);
-                calculationRepository.save(calc);
-            }
-//            calculations.put("liftA1", weekOne(max.getDeadLift()).get(0));
-//            calculations.put("liftA2", weekOne(max.getDeadLift()).get(1));
-//            calculations.put("liftA3", weekOne(max.getDeadLift()).get(2));
-            for (int i : weekOne(max.getDeadLift())) {
-                Calculation calc = new Calculation(i, person);
-                calculationRepository.save(calc);
-            }
-//            calculations.put("benchB1", weekTwo(max.getBench()).get(0));
-//            calculations.put("benchB2", weekTwo(max.getBench()).get(1));
-//            calculations.put("benchB3", weekTwo(max.getBench()).get(2));
-            for (int i : weekTwo(max.getBench())) {
-                Calculation calc = new Calculation(i, person);
-                calculationRepository.save(calc);
-            }
-//            calculations.put("pressB1", weekTwo(max.getShoulderPress()).get(0));
-//            calculations.put("pressB2", weekTwo(max.getShoulderPress()).get(1));
-//            calculations.put("pressB3", weekTwo(max.getShoulderPress()).get(2));
-            for (int i : weekTwo(max.getShoulderPress())) {
-                Calculation calc = new Calculation(i, person);
-                calculationRepository.save(calc);
-            }
-//            calculations.put("squatB1", weekTwo(max.getSquat()).get(0));
-//            calculations.put("squatB2", weekTwo(max.getSquat()).get(1));
-//            calculations.put("squatB3", weekTwo(max.getSquat()).get(2));
-            for (int i : weekTwo(max.getSquat())) {
-                Calculation calc = new Calculation(i, person);
-                calculationRepository.save(calc);
-            }
-//            calculations.put("liftB1", weekTwo(max.getDeadLift()).get(0));
-//            calculations.put("liftB2", weekTwo(max.getDeadLift()).get(1));
-//            calculations.put("liftB3", weekTwo(max.getDeadLift()).get(2));
-            for (int i : weekTwo(max.getDeadLift())) {
-                Calculation calc = new Calculation(i, person);
-                calculationRepository.save(calc);
-            }
-//            calculations.put("benchC1", weekThree(max.getBench()).get(0));
-//            calculations.put("benchC2", weekThree(max.getBench()).get(1));
-//            calculations.put("benchC3", weekThree(max.getBench()).get(2));
-            for (int i : weekThree(max.getBench())) {
-                Calculation calc = new Calculation(i, person);
-                calculationRepository.save(calc);
-            }
-//            calculations.put("pressC1", weekThree(max.getShoulderPress()).get(0));
-//            calculations.put("pressC2", weekThree(max.getShoulderPress()).get(1));
-//            calculations.put("pressC3", weekThree(max.getShoulderPress()).get(2));
-            for (int i : weekThree(max.getShoulderPress())) {
-                Calculation calc = new Calculation(i, person);
-                calculationRepository.save(calc);
-            }
-//            calculations.put("squatC1", weekThree(max.getSquat()).get(0));
-//            calculations.put("squatC2", weekThree(max.getSquat()).get(1));
-//            calculations.put("squatC3", weekThree(max.getSquat()).get(2));
-            for (int i : weekThree(max.getSquat())) {
-                Calculation calc = new Calculation(i, person);
-                calculationRepository.save(calc);
-            }
-//            calculations.put("liftC1", weekThree(max.getDeadLift()).get(0));
-//            calculations.put("liftC2", weekThree(max.getDeadLift()).get(1));
-//            calculations.put("liftC3", weekThree(max.getDeadLift()).get(2));
-            for (int i : weekThree(max.getDeadLift())) {
-                Calculation calc = new Calculation(i, person);
-                calculationRepository.save(calc);
-            }
-//            calculations.put("benchD1", weekFour(max.getBench()).get(0));
-//            calculations.put("benchD2", weekFour(max.getBench()).get(1));
-//            calculations.put("benchD3", weekFour(max.getBench()).get(2));
-            for (int i : weekFour(max.getBench())) {
-                Calculation calc = new Calculation(i, person);
-                calculationRepository.save(calc);
-            }
-//            calculations.put("pressD1", weekFour(max.getShoulderPress()).get(0));
-//            calculations.put("pressD2", weekFour(max.getShoulderPress()).get(1));
-//            calculations.put("pressD3", weekFour(max.getShoulderPress()).get(2));
-            for (int i : weekFour(max.getShoulderPress())) {
-                Calculation calc = new Calculation(i, person);
-                calculationRepository.save(calc);
-            }
-//            calculations.put("squatD1", weekFour(max.getSquat()).get(0));
-//            calculations.put("squatD2", weekFour(max.getSquat()).get(1));
-//            calculations.put("squatD3", weekFour(max.getSquat()).get(2));
-            for (int i : weekFour(max.getSquat())) {
-                Calculation calc = new Calculation(i, person);
-                calculationRepository.save(calc);
-            }
-//            calculations.put("liftD1", weekFour(max.getDeadLift()).get(0));
-//            calculations.put("liftD2", weekFour(max.getDeadLift()).get(1));
-//            calculations.put("liftD3", weekFour(max.getDeadLift()).get(2));
-            for (int i : weekFour(max.getDeadLift())) {
-                Calculation calc = new Calculation(i, person);
-                calculationRepository.save(calc);
-            }
-            model.addAttribute("calculated", calculationRepository.findByPerson(person));
+            calculations.put("benchA1", weekOne(max.getBench()).get(0));
+            calculations.put("benchA2", weekOne(max.getBench()).get(1));
+            calculations.put("benchA3", weekOne(max.getBench()).get(2));
+//            for (int i : weekOne(max.getBench())) {
+//                Calculation calc = new Calculation(i, person);
+//                calculationRepository.save(calc);
+//            }
+            calculations.put("pressA1", weekOne(max.getShoulderPress()).get(0));
+            calculations.put("pressA2", weekOne(max.getShoulderPress()).get(1));
+            calculations.put("pressA3", weekOne(max.getShoulderPress()).get(2));
+//            for (int i : weekOne(max.getShoulderPress())) {
+//                Calculation calc = new Calculation(i, person);
+//                calculationRepository.save(calc);
+//            }
+            calculations.put("squatA1", weekOne(max.getSquat()).get(0));
+            calculations.put("squatA2", weekOne(max.getSquat()).get(1));
+            calculations.put("squatA3", weekOne(max.getSquat()).get(2));
+//            for (int i : weekOne(max.getSquat())) {
+//                Calculation calc = new Calculation(i, person);
+//                calculationRepository.save(calc);
+//            }
+            calculations.put("liftA1", weekOne(max.getDeadLift()).get(0));
+            calculations.put("liftA2", weekOne(max.getDeadLift()).get(1));
+            calculations.put("liftA3", weekOne(max.getDeadLift()).get(2));
+//            for (int i : weekOne(max.getDeadLift())) {
+//                Calculation calc = new Calculation(i, person);
+//                calculationRepository.save(calc);
+//            }
+            calculations.put("benchB1", weekTwo(max.getBench()).get(0));
+            calculations.put("benchB2", weekTwo(max.getBench()).get(1));
+            calculations.put("benchB3", weekTwo(max.getBench()).get(2));
+//            for (int i : weekTwo(max.getBench())) {
+//                Calculation calc = new Calculation(i, person);
+//                calculationRepository.save(calc);
+//            }
+            calculations.put("pressB1", weekTwo(max.getShoulderPress()).get(0));
+            calculations.put("pressB2", weekTwo(max.getShoulderPress()).get(1));
+            calculations.put("pressB3", weekTwo(max.getShoulderPress()).get(2));
+//            for (int i : weekTwo(max.getShoulderPress())) {
+//                Calculation calc = new Calculation(i, person);
+//                calculationRepository.save(calc);
+//            }
+            calculations.put("squatB1", weekTwo(max.getSquat()).get(0));
+            calculations.put("squatB2", weekTwo(max.getSquat()).get(1));
+            calculations.put("squatB3", weekTwo(max.getSquat()).get(2));
+//            for (int i : weekTwo(max.getSquat())) {
+//                Calculation calc = new Calculation(i, person);
+//                calculationRepository.save(calc);
+//            }
+            calculations.put("liftB1", weekTwo(max.getDeadLift()).get(0));
+            calculations.put("liftB2", weekTwo(max.getDeadLift()).get(1));
+            calculations.put("liftB3", weekTwo(max.getDeadLift()).get(2));
+//            for (int i : weekTwo(max.getDeadLift())) {
+//                Calculation calc = new Calculation(i, person);
+//                calculationRepository.save(calc);
+//            }
+            calculations.put("benchC1", weekThree(max.getBench()).get(0));
+            calculations.put("benchC2", weekThree(max.getBench()).get(1));
+            calculations.put("benchC3", weekThree(max.getBench()).get(2));
+//            for (int i : weekThree(max.getBench())) {
+//                Calculation calc = new Calculation(i, person);
+//                calculationRepository.save(calc);
+//            }
+            calculations.put("pressC1", weekThree(max.getShoulderPress()).get(0));
+            calculations.put("pressC2", weekThree(max.getShoulderPress()).get(1));
+            calculations.put("pressC3", weekThree(max.getShoulderPress()).get(2));
+//            for (int i : weekThree(max.getShoulderPress())) {
+//                Calculation calc = new Calculation(i, person);
+//                calculationRepository.save(calc);
+//            }
+            calculations.put("squatC1", weekThree(max.getSquat()).get(0));
+            calculations.put("squatC2", weekThree(max.getSquat()).get(1));
+            calculations.put("squatC3", weekThree(max.getSquat()).get(2));
+//            for (int i : weekThree(max.getSquat())) {
+//                Calculation calc = new Calculation(i, person);
+//                calculationRepository.save(calc);
+//            }
+            calculations.put("liftC1", weekThree(max.getDeadLift()).get(0));
+            calculations.put("liftC2", weekThree(max.getDeadLift()).get(1));
+            calculations.put("liftC3", weekThree(max.getDeadLift()).get(2));
+//            for (int i : weekThree(max.getDeadLift())) {
+//                Calculation calc = new Calculation(i, person);
+//                calculationRepository.save(calc);
+//            }
+            calculations.put("benchD1", weekFour(max.getBench()).get(0));
+            calculations.put("benchD2", weekFour(max.getBench()).get(1));
+            calculations.put("benchD3", weekFour(max.getBench()).get(2));
+//            for (int i : weekFour(max.getBench())) {
+//                Calculation calc = new Calculation(i, person);
+//                calculationRepository.save(calc);
+//            }
+            calculations.put("pressD1", weekFour(max.getShoulderPress()).get(0));
+            calculations.put("pressD2", weekFour(max.getShoulderPress()).get(1));
+            calculations.put("pressD3", weekFour(max.getShoulderPress()).get(2));
+//            for (int i : weekFour(max.getShoulderPress())) {
+//                Calculation calc = new Calculation(i, person);
+//                calculationRepository.save(calc);
+//            }
+            calculations.put("squatD1", weekFour(max.getSquat()).get(0));
+            calculations.put("squatD2", weekFour(max.getSquat()).get(1));
+            calculations.put("squatD3", weekFour(max.getSquat()).get(2));
+//            for (int i : weekFour(max.getSquat())) {
+//                Calculation calc = new Calculation(i, person);
+//                calculationRepository.save(calc);
+//            }
+            calculations.put("liftD1", weekFour(max.getDeadLift()).get(0));
+            calculations.put("liftD2", weekFour(max.getDeadLift()).get(1));
+            calculations.put("liftD3", weekFour(max.getDeadLift()).get(2));
+//            for (int i : weekFour(max.getDeadLift())) {
+//                Calculation calc = new Calculation(i, person);
+//                calculationRepository.save(calc);
+//            }
+            model.addAttribute("calculated", calculations);
+        }
+        if (noteRepository.findByPerson(person) != null) {
+            model.addAttribute("note", noteRepository.findByPerson(person));
         }
         model.addAttribute("person", person);
         return "home";
@@ -196,14 +198,13 @@ public class FiveThreeOneController {
         }
         return "redirect:/";
     }
-//    @RequestMapping(path = "/addNote", method = RequestMethod.POST)
-//    public String addNote(HttpSession session, String text) {
-//        String userName = (String) session.getAttribute("userName");
-//        Note note = new Note(text, personRepository.findFirstByUserName(userName));
-//        noteRepository.save(note);
-//        return "redirect:/";
-//    }
-
+    @RequestMapping(path = "/addNote", method = RequestMethod.POST)
+    public String addNote(HttpSession session, String text) {
+        String userName = (String) session.getAttribute("userName");
+        Note note = new Note(text, personRepository.findFirstByUserName(userName));
+        noteRepository.save(note);
+        return "redirect:/";
+    }
     static ArrayList<Integer> weekOne (int amt) {
         int a1 = (int) Math.round(amt*.65);
         int a2 = (int) Math.round(amt*.75);
--- src/main/java/com/theironyard/services/NoteRepository.java
+++ src/main/java/com/theironyard/services/NoteRepository.java
@@ -1,8 +1,7 @@
 package com.theironyard.services;
 
-import com.theironyard.entities.Calculation;
+import com.theironyard.entities.Note;
 import com.theironyard.entities.Person;
-
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -10,6 +9,6 @@ import java.util.List;
 /**
  * Created by noellemachin on 3/14/16.
  */
-public interface CalculationRepository extends CrudRepository<Calculation, Integer> {
-    List<Calculation> findByPerson(Person person);
+public interface NoteRepository extends CrudRepository<Note, Integer> {
+    List<Note> findByPerson(Person person);
 }
